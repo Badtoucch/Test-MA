dc92ef0df2e34d91a4d6eede4f89c42d2354d662
Vladislad T <v.trundenko@everad.com>
2019-09-15 23:36:41 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const fruits = ["Banana", "Apple", "Orange", "Lemon"];
 
 function fn(list, nameFruit) {
-  for (let i = 0; i <= list.length - 1; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] == nameFruit) {
       list[i] = "Only " + list[i];
     }
